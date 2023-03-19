49267f06ffa3a62387441d1918a461617edbd621
chadda <chadha.essid@insat.ucar.tn>
2023-03-19 22:01:26 +0100

    index

---
+++ index.js
@@ -0,0 +1,22 @@
+let butt=document.querySelector(".btn.btn-primary");
+butt.addEventListener("mouseover",()=>
+{   setTimeout(()=>
+        {butt.style.border = "2px solid gold";
+        butt.style.backgroundColor="gold";
+        butt.style.color="black";
+        butt.textContent="view my work ↓"; 
+        },1)
+
+
+}) 
+
+
+
+butt.addEventListener("click", function() {
+    window.location.href = "about.html";
+  });
+butt.addEventListener("mouseout",()=>{butt.style.backgroundColor= "black";
+butt.textContent="view my work →";
+butt.style.border = "";
+butt.style.color="";})
+
