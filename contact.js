0aa334b8409c6ca77bacaac4c8b305134d8fff4b
chadda <chadha.essid@insat.ucar.tn>
2023-03-20 19:19:03 +0100

    added the contact page

---
+++ contact.js
@@ -0,0 +1,20 @@
+let picture=document.querySelector("#backToTop"); 
+
+
+
+addEventListener("mouseover",(e)=>{
+    if(e.target.matches("img")&& e.target!==picture) 
+    e.target.style.height="103%";
+    e.target.style.wifth="103%";
+
+})
+
+
+addEventListener("mouseout",(e)=>{
+    if(e.target.matches("img"))
+    e.target.style.height="";
+    e.target.style.wifth="";
+
+}) 
+
+
